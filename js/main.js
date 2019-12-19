29092e033889def8bda5f37941b82109ed473578
lily-shen <921180142@qq.com>
2019-12-19 20:30:41 +0800

    Signed-off-by: lily-shen <921180142@qq.com>

--- js/main.js
+++ js/main.js
@@ -48,17 +48,7 @@ $(function () {
       $('.navbar-collapse').toggleClass('active');
     });
 
-    $(window).scroll(function () {
-      //创建一个变量存储当前窗口下移的高度
-      var scroTop = $(window).scrollTop();
-      //如果大于100，则显示顶部元素，否则隐藏顶部元素
-      if (scroTop > 100) {
-        $('header').addClass('active')
-      } else {
-        $('header').removeClass('active')
-      }
-    }
-    );
+   
 
 
   })
